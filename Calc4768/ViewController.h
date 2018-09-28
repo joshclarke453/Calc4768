1ff760f61ce58f884d6ca6ead9f0942818c11b6a
Joshua <joshuaxcode@grannysmith.cs.mun.ca>
2018-09-28 10:44:10 -0230

    Initial Commit

---
+++ Calc4768/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calc4768
+//
+//  Created by Joshua on 2018-09-28.
+//  Copyright © 2018 jtc260. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calc4768/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calc4768
+//
+//  Created by Joshua on 2018-09-28.
+//  Copyright © 2018 jtc260. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
