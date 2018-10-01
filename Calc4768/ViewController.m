2b970037b80acadd76a458085344207c869d901e
Joshua <joshuaxcode@grannysmith.cs.mun.ca>
2018-10-01 20:10:18 -0230

    Added functionality to rest of buttons, created the ability to do repeated operations without hitting the equals sign, did some minor bugfixes and cleaned up code

--- Calc4768/ViewController.m
+++ Calc4768/ViewController.m
@@ -15,7 +15,8 @@
 @end
 
 @implementation ViewController
-//NSString *tempString = nil;
+NSString *tempNum = nil;
+NSString *operand = nil;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -26,13 +27,19 @@
     _textField.text = [_textField.text stringByAppendingString:input.currentTitle];
 }
 
-//Fix this before subitting, it should cancel any actions aswell
 - (IBAction)clearContents:(UIButton *)sender {
-    _textField.text = nil;
+    if (_textField.text != nil) {
+        _textField.text = nil;
+    } else {
+        tempNum = nil;
+        operand = nil;
+    }
 }
 
 - (IBAction)deleteInput:(UIButton *)input {
-    _textField.text = [_textField.text substringToIndex:_textField.text.length-1];
+    if (_textField.text.length >= 1) {
+        _textField.text = [_textField.text substringToIndex:_textField.text.length-1];
+    }
 }
 
 - (IBAction)xSquared:(UIButton *)sender {
@@ -59,10 +66,99 @@
     _textField.text = [NSString stringWithFormat:@"%g",x];
 }
 
-//- (IBAction)add {
-//    tempString = _textField.text;
-//
-//}
+//1+2-3= doesnt work, neither does 1+3/2^4, etc..
+//but 1+1+1+1 does...
+- (IBAction)add {
+    if (tempNum == nil) {
+        tempNum = _textField.text;
+        operand = @"+";
+        _textField.text = nil;
+    } else if ([operand  isEqual: @"+"]) {
+        double x = [tempNum doubleValue] + [_textField.text doubleValue];
+        tempNum = [NSString stringWithFormat:@"%g",x];
+        _textField.text = nil;
+    } else {
+        [self computeEquation];
+        tempNum = _textField.text;
+        _textField.text = nil;
+        operand = @"+";
+    }
+}
+
+- (IBAction)subtract {
+    if (tempNum == nil) {
+        tempNum = _textField.text;
+        operand = @"-";
+        _textField.text = nil;
+    } else {
+        double x = [tempNum doubleValue] - [_textField.text doubleValue];
+        tempNum = [NSString stringWithFormat:@"%g",x];
+        _textField.text = nil;
+    }
+}
+- (IBAction)xPowY {
+    if (tempNum == nil) {
+        tempNum = _textField.text;
+        operand = @"^";
+        _textField.text = nil;
+    } else {
+        double x = pow([tempNum doubleValue] ,[_textField.text doubleValue]);
+        tempNum = [NSString stringWithFormat:@"%g",x];
+        _textField.text = nil;
+    }
+}
+
+- (IBAction)multiply {
+    if (tempNum == nil) {
+        tempNum = _textField.text;
+        operand = @"x";
+        _textField.text = nil;
+    } else {
+        double x = [tempNum doubleValue]*[_textField.text doubleValue];
+        tempNum = [NSString stringWithFormat:@"%g",x];
+        _textField.text = nil;
+    }
+}
+
+- (IBAction)divide {
+    if (tempNum == nil) {
+        tempNum = _textField.text;
+        operand = @"/";
+        _textField.text = nil;
+    } else {
+        double x = [tempNum doubleValue]/[_textField.text doubleValue];
+        tempNum = [NSString stringWithFormat:@"%g",x];
+        _textField.text = nil;
+    }
+}
+
+- (IBAction)plusEquals {
+    double x = [_textField.text doubleValue]+[_textField.text doubleValue];
+    _textField.text = [NSString stringWithFormat:@"%g",x];
+}
+
+
+- (IBAction)computeEquation {
+    if ([operand  isEqual: @"+"]) {
+        double x = [tempNum doubleValue] + [_textField.text doubleValue];
+        _textField.text = [NSString stringWithFormat:@"%g",x];
+    } else if ([operand isEqual:@"-"]) {
+        double x = [tempNum doubleValue] - [_textField.text doubleValue];
+        _textField.text = [NSString stringWithFormat:@"%g",x];
+    } else if ([operand isEqual:@"^"]) {
+        double x = pow([tempNum doubleValue], [_textField.text doubleValue]);
+        _textField.text = [NSString stringWithFormat:@"%g",x];
+    } else if ([operand isEqual:@"x"]) {
+        double x = [tempNum doubleValue]*[_textField.text doubleValue];
+        _textField.text = [NSString stringWithFormat:@"%g",x];
+    } else if ([operand isEqual:@"/"]) {
+        double x = [tempNum doubleValue]/[_textField.text doubleValue];
+        _textField.text = [NSString stringWithFormat:@"%g",x];
+    }
+    tempNum = nil;
+    operand = nil;
+}
 
 
 @end
+
