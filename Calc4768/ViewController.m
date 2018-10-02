deaf63eaf0717241c3ef9c4e3f5ffaa2b5a60e2e
Joshua <joshuaxcode@grannysmith.cs.mun.ca>
2018-10-02 10:04:48 -0230

    Spelling and grammar fixes in comments.

--- Calc4768/ViewController.m
+++ Calc4768/ViewController.m
@@ -24,14 +24,14 @@ NSString *operand = nil;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-//This method will take any number buton pressed and append the content into the textfield.
+//This method will take any number button pressed and append the content into the textfield.
 - (IBAction)numButPressed:(UIButton *)input {
     _textField.text = [_textField.text stringByAppendingString:input.currentTitle];
 }
 
-//This method is for the "C" button
+//This method is for the "C" button.
 //If clicked, it will clear the textfield.
-//If the textfield is empty when it is clicked, it will cancel any operation that may have been clicked
+//If the textfield is empty when it is clicked, it will cancel any operation that may have been clicked.
 - (IBAction)clearContents:(UIButton *)sender {
     if (_textField.text != nil) {
         _textField.text = nil;
@@ -57,7 +57,7 @@ NSString *operand = nil;
     _textField.text = [NSString stringWithFormat:@"%g",x];
 }
 
-//This is the method for "x^3"
+//This is the method for "x^3".
 //It will cube whatever is in the textfield.
 - (IBAction)xCubed:(UIButton *)sender {
     double x = [_textField.text doubleValue];
@@ -66,7 +66,7 @@ NSString *operand = nil;
 }
 
 //This is the method for "√".
-//It will get the square rot of whatever is in the textfield.
+//It will get the square root of whatever is in the textfield.
 - (IBAction)squareRootOfX:(UIButton *)sender {
     double x = [_textField.text doubleValue];
     x = sqrt(x);
@@ -83,7 +83,7 @@ NSString *operand = nil;
 
 //This is the method for "+"
 //If no operation has been selected yet, it will take the contents of the textfield and put them in a temporary variable to hold, as well as putting the 'operand' variable equal to '+'.
-//If this is the second click of the same operation before the '=' button is clicked, it will coplete the operation and set the tempNum variable to the answer. This allows users to do multiple operations in sequence without having to hit the '=' key, such as user inputs like '1+1+1+1' before hitting '=' will work and give the user the correct answer.
+//If this is the second click of the same operation before the '=' button is clicked, it will complete the operation and set the tempNum variable to the answer. This allows users to do multiple operations in sequence without having to hit the '=' key, such as user inputs like '1+1+1+1' before hitting '=' will work and give the user the correct answer.
 //If this is the second click, but of different operations, it will call the 'computeEquation' function to do the equation before functioning, this allows users to do inputs such as '1+3-4x7' in sequence without hitting the '=' key, and will give the correct answer.
 //All methods requiring multiple number inputs function in a similar fashion, with mild tweaks as needed.
 - (IBAction)add {
@@ -179,7 +179,7 @@ NSString *operand = nil;
 }
 
 //This is the '=' method.
-//It acts as a completion for the methods which require 2 numerical inputs. It simply takes the first input, stored in 'tempNum' and the second input, stored in the textfield, and checks what operator should go between them, which is stored in 'operand', then preforms the calculation and puts it in the textfield, then clears 'tempNum' and 'operand'.
+//It acts as a completion for the methods which require 2 numerical inputs. It simply takes the first input, stored in 'tempNum' and the second input, stored in the textfield, and checks what operator should go between them, which is stored in 'operand', then performs the calculation and puts it in the textfield, then clears 'tempNum' and 'operand'.
 - (IBAction)computeEquation {
     if ([operand  isEqual: @"+"]) {
         double x = [tempNum doubleValue] + [_textField.text doubleValue];
